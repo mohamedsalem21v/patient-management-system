40f1013711dca0204d4978f3d75489f31cfc31af
Moza <moazw9969@gmail.com>
2025-12-01 22:55:04 +0200

    intial commit

--- Patient.cpp
+++ Patient.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 // Enum for case types
-enum CaseType {
+enum CaseType
+{
     GENERAL,
     EMERGENCY,
     ICU,
@@ -10,7 +11,8 @@ enum CaseType {
     SURGICAL
 };
 
-class Patient {
+class Patient
+{
 private:
     int id;
     string name;
@@ -19,62 +21,96 @@ private:
 
 public:
     // Empty constructor
-    Patient() : id(0), name(""), age(0), caseType(GENERAL) {}
+    Patient()
+    {
+        id = 0;
+        name = "";
+        age = 0;
+        caseType = GENERAL;
+    }
 
     // Parameterized constructor
-    Patient(int patientId, string patientName, int patientAge, CaseType patientCaseType) 
-        : id(patientId), name(patientName), age(patientAge), caseType(patientCaseType) {}
+    Patient(int patientId, string patientName, int patientAge, CaseType patientCaseType)
+    {
+        id = patientId;
+        name = patientName;
+        age = patientAge;
+        caseType = patientCaseType;
+    }
 
     // Display patient information
-    void display() const {
+    void display() const
+    {
         string caseTypeStr;
-        switch(caseType) {
-            case GENERAL: caseTypeStr = "GENERAL"; break;
-            case EMERGENCY: caseTypeStr = "EMERGENCY"; break;
-            case ICU: caseTypeStr = "ICU"; break;
-            case PEDIATRIC: caseTypeStr = "PEDIATRIC"; break;
-            case SURGICAL: caseTypeStr = "SURGICAL"; break;
-            default: caseTypeStr = "UNKNOWN";
+        switch (caseType)
+        {
+        case GENERAL:
+            caseTypeStr = "GENERAL";
+            break;
+        case EMERGENCY:
+            caseTypeStr = "EMERGENCY";
+            break;
+        case ICU:
+            caseTypeStr = "ICU";
+            break;
+        case PEDIATRIC:
+            caseTypeStr = "PEDIATRIC";
+            break;
+        case SURGICAL:
+            caseTypeStr = "SURGICAL";
+            break;
+        default:
+            caseTypeStr = "UNKNOWN";
         }
-        
-        cout << "Patient ID: " << id << endl;
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-        cout << "Case Type: " << caseTypeStr << endl;
-        cout << "------------------------" << endl;
+
+        cout << "Patient ID: " << id << '\n';
+        cout << "Name: " << name << '\n';
+        cout << "Age: " << age << '\n';
+        cout << "Case Type: " << caseTypeStr << '\n';
+        cout << "------------------------" << '\n';
     }
 
     // Getters
-    int getId() const {
+    int getId() const
+    {
         return id;
     }
 
-    string getName() const {
+    string getName() const
+    {
         return name;
     }
 
-    int getAge() const {
+    int getAge() const
+    {
         return age;
     }
 
-    CaseType getCaseType() const {
+    CaseType getCaseType() const
+    {
         return caseType;
     }
 
     // Setters
-    void setName(const string& newName) {
+    void setName(const string &newName)
+    {
         name = newName;
     }
 
-    void setAge(int newAge) {
-        if (newAge >= 0) {
+    void setAge(int newAge)
+    {
+        if (newAge >= 0)
+        {
             age = newAge;
-        } else {
+        }
+        else
+        {
             cout << "Error: Age cannot be negative!" << endl;
         }
     }
 
-    void setCaseType(CaseType newCaseType) {
+    void setCaseType(CaseType newCaseType)
+    {
         caseType = newCaseType;
     }
 };
